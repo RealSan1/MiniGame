d922076c999bb685c10fb48284c67a9b2bb627f8
San <soltks123@gmail.com>
2024-04-09 22:52:16 +0900

    버튼 연결

--- app/src/main/java/com/inhatc/minigame_application/SelectGameActivity.java
+++ app/src/main/java/com/inhatc/minigame_application/SelectGameActivity.java
@@ -1,6 +1,8 @@
 package com.inhatc.minigame_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class SelectGameActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Find_Color(View view) {
+        Intent intent = new Intent(getApplicationContext(), FindColor.class);
+        startActivity(intent);
     }
 }
