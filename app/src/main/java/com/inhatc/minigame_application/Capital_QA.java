7f85240305813c47b3a31c7102d966a9683c770a
SooHyun Nam <nsh1925@naver.com>
2024-05-06 08:36:24 +0900

    DB서버 미완 안드로이드 내장 DB SQLite사용 클래스 구현

--- app/src/main/java/com/inhatc/minigame_application/Capital_QA.java
+++ app/src/main/java/com/inhatc/minigame_application/Capital_QA.java
@@ -1,6 +1,8 @@
 package com.inhatc.minigame_application;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class Capital_QA extends AppCompatActivity {
     private static DBHelper dbHelper;
+    private static TextView question;
+    private static TextView answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +27,23 @@ public class Capital_QA extends AppCompatActivity {
         dbHelper = DBHelper.getInstance(getApplicationContext());
         insert_qa(dbHelper);
 
-        //DB데이터 가져오기
+        System.out.println("capital test");
 
+        //DB데이터 가져오기
+        Cursor cursor = dbHelper.getAllData();
+        if(cursor.moveToFirst()){
+        int quesitonIndex = cursor.getColumnIndex("qa_question");
+        int answerIndex = cursor.getColumnIndex("qa_answer");
+
+        String questions = cursor.getString(quesitonIndex);
+        String answers = cursor.getString(answerIndex);
+
+        question = (TextView)findViewById(R.id.textView2);
+        question.setText(questions);
+
+        answer = (TextView)findViewById(R.id.textView3);
+        answer.setText(answers);
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
