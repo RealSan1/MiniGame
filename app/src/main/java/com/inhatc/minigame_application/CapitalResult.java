28c3ef2ec1e4a9a6231a734738703d5650a92287
IMBLOP <eogur001@naver.com>
2024-05-17 02:08:20 +0900

    수도 맞히기 사진 출력 제외 완성

--- app/src/main/java/com/inhatc/minigame_application/CapitalResult.java
+++ app/src/main/java/com/inhatc/minigame_application/CapitalResult.java
@@ -17,16 +17,16 @@ public class CapitalResult extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_capital_result);
 
-        int correct = getIntent().getIntExtra("correct",1);
-        int total = getIntent().getIntExtra("total",1);
+        int correct = getIntent().getIntExtra("correct", 0);
+        int total = getIntent().getIntExtra("total", 1);
 
-        int score = (int)(correct/total)*100;
+        int score = (int) (((double) correct / total) * 100);
 
-        TextView result = (TextView)findViewById(R.id.txtCapResult);
-        TextView scorePer = (TextView)findViewById(R.id.txtCapScore);
+        TextView result = findViewById(R.id.txtCapResult);
+        TextView scorePer = findViewById(R.id.txtCapScore);
 
         result.setText(correct + "개 맞히셨습니다.");
-        scorePer.setText(score);
+        scorePer.setText(score + "%"); // 점수 뒤에 % 추가
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
